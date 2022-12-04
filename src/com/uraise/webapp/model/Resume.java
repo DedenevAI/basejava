b212d0d6c5470ba1bbe650c574eac5de9aebc061
Dedenev <dedenevai3263@yandex.ru>
2022-12-04 20:17:58 +0300

    Added equals&hashCode method to Resume

--- src/com/uraise/webapp/model/Resume.java
+++ src/com/uraise/webapp/model/Resume.java
@@ -1,5 +1,7 @@
 package com.uraise.webapp.model;
 
+import java.util.Objects;
+
 /**
  * Initial resume class
  */
@@ -8,16 +10,26 @@ public class Resume {
     // Unique identifier
     private String uuid;
 
-    @Override
-    public String toString() {
-        return uuid;
-    }
-
     public String getUuid() {
         return uuid;
     }
 
     public void setUuid(String uuid) {
         this.uuid = uuid;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Resume resume = (Resume) o;
+        return Objects.equals(uuid, resume.uuid);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(uuid);
+    }
+    @Override
+    public String toString() {
+        return uuid;
     }
 }
