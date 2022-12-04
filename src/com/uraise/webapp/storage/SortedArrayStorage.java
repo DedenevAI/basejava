fed660455ead76034c2ca5d94d5ed4d394d41605
Dedenev <dedenevai3263@yandex.ru>
2022-12-04 20:45:44 +0300

    Realize getIndex method in SortedArrayStorage

---
+++ src/com/uraise/webapp/storage/SortedArrayStorage.java
@@ -0,0 +1,44 @@
+package com.uraise.webapp.storage;
+
+import com.uraise.webapp.model.Resume;
+
+import java.util.Arrays;
+
+public class SortedArrayStorage extends AbstractArrayStorage {
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void save(Resume r) {
+
+    }
+
+    @Override
+    public void update(Resume r) {
+
+    }
+
+    @Override
+    public void delete(String uuid) {
+
+    }
+
+    @Override
+    public Resume[] getAll() {
+        return new Resume[0];
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    protected int getIndex(String uuid) {
+        Resume searchKey = new Resume();
+        searchKey.setUuid(uuid);
+        return Arrays.binarySearch(storage, 0, size, searchKey);
+    }
+}
