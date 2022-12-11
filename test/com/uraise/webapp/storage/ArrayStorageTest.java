c8b5a459133ed7092f158a67f2261fe3999f5ab4
Dedenev <dedenevai3263@yandex.ru>
2022-12-11 17:23:32 +0300

    Added JUnit-test

---
+++ test/com/uraise/webapp/storage/ArrayStorageTest.java
@@ -0,0 +1,9 @@
+package com.uraise.webapp.storage;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ArrayStorageTest extends AbstractArrayStorageTest {
+
+}
---
+++ test/com/uraise/webapp/storage/SortedArrayStorageTest.java
@@ -0,0 +1,7 @@
+package com.uraise.webapp.storage;
+
+import static org.junit.Assert.*;
+
+public class SortedArrayStorageTest extends AbstractArrayStorageTest {
+
+}
