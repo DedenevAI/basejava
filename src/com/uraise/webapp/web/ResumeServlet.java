aa77545ea61a4c13cd2602bc8b6f0ea2a143bb67
Dedenev <dedenevai3263@yandex.ru>
2023-02-04 23:11:56 +0300

    HW 15 servlet completed

--- src/com/uraise/webapp/web/ResumeServlet.java
+++ src/com/uraise/webapp/web/ResumeServlet.java
@@ -15,6 +15,7 @@ import java.io.Writer;
 
 public class ResumeServlet extends HttpServlet {
     private Storage storage;
+
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
@@ -27,31 +28,11 @@ public class ResumeServlet extends HttpServlet {
         response.setCharacterEncoding("UTF-8");
         response.setHeader("Content-Type", "text/html; character=UTF-8");
         Writer writer = response.getWriter();
-        writer.write(
-                "<html>\n" +
-                        "<head>\n" +
-                        "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n" +
-                        "    <link rel=\"stylesheet\" href=\"css/style.css\">\n" +
-                        "    <title>Список всех резюме</title>\n" +
-                        "</head>\n" +
-                        "<body>\n" +
-                        "<section>\n" +
-                        "<table border=\"1\" cellpadding=\"8\" cellspacing=\"0\">\n" +
-                        "    <tr>\n" +
-                        "        <th>Имя</th>\n" +
-                        "        <th>Email</th>\n" +
-                        "    </tr>\n");
+        writer.write("<html>\n" + "<head>\n" + "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n" + "    <link rel=\"stylesheet\" href=\"css/style.css\">\n" + "    <title>Список всех резюме</title>\n" + "</head>\n" + "<body>\n" + "<section>\n" + "<table border=\"1\" cellpadding=\"8\" cellspacing=\"0\">\n" + "    <tr>\n" + "        <th>Имя</th>\n" + "        <th>Email</th>\n" + "    </tr>\n");
         for (Resume resume : storage.getAllSorted()) {
-            writer.write(
-                    "<tr>\n" +
-                            "     <td><a href=\"resume?uuid=" + resume.getUuid() + "\">" + resume.getFullName() + "</a></td>\n" +
-                            "     <td>" + resume.getContact(ContactType.EMAIL) + "</td>\n" +
-                            "</tr>\n");
+            writer.write("<tr>\n" + "     <td><a href=\"resume?uuid=" + resume.getUuid() + "\">" + resume.getFullName() + "</a></td>\n" + "     <td>" + resume.getContact(ContactType.EMAIL) + "</td>\n" + "</tr>\n");
         }
-        writer.write("</table>\n" +
-                "</section>\n" +
-                "</body>\n" +
-                "</html>\n");
+        writer.write("</table>\n" + "</section>\n" + "</body>\n" + "</html>\n");
     }
 
 
